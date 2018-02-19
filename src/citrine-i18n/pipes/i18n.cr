4e31894c5ea706e1d816708f90e3bce8864771cf
Davor Ocelic <docelic@spinlocksolutions.com>
2018-02-19 12:04:42 +0100

    Remove dependency on Amber

--- src/citrine-i18n/pipes/i18n.cr
+++ src/citrine-i18n/pipes/i18n.cr
@@ -1,17 +1,18 @@
 module Citrine
   module Pipe
     # Handler to initialize I18n for the request.
-    class I18n < Amber::Pipe::Base
-			HEADER = "Accept-Language"
+    class I18n
+      include HTTP::Handler
+      HEADER = "Accept-Language"
 
       def call(context : HTTP::Server::Context)
-				if languages = context.request.headers[HEADER]?
-					parser = Citrine::I18n::Parser.new languages
-					compat = parser.compatible_language_from ::I18n.available_locales
-					::I18n.locale = compat if compat
-					#Amber.logger.debug "Languages available: #{languages.to_s}"
-					#Amber.logger.debug "Language chosen: #{::I18n.locale}"
-				end
+        if languages = context.request.headers[HEADER]?
+          parser = Citrine::I18n::Parser.new languages
+          compat = parser.compatible_language_from ::I18n.available_locales
+          ::I18n.locale = compat if compat
+          #Amber.logger.debug "Languages available: #{languages.to_s}"
+          #Amber.logger.debug "Language chosen: #{::I18n.locale}"
+        end
         call_next(context)
       end
     end
